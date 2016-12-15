b06c4eac69db4b5fc2d23d98a7be276c654c66f4
sakethkaparthi <sakethkaparthi@outlook.com>
2016-12-15 18:23:35 +0530

    Log event in about acitivity

--- app/src/main/java/sakethkaparthi/fileio/activities/AboutActivity.java
+++ app/src/main/java/sakethkaparthi/fileio/activities/AboutActivity.java
@@ -3,13 +3,22 @@ package sakethkaparthi.fileio.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import sakethkaparthi.fileio.R;
+import sakethkaparthi.fileio.applications.FileIOApplication;
 
 public class AboutActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        mFirebaseAnalytics = ((FileIOApplication) getApplication()).getAnalyticsInstance();
+        Bundle params = new Bundle();
+        params.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "screen");
+        params.putString(FirebaseAnalytics.Param.ITEM_NAME, "About Activity");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM, params);
     }
 }
